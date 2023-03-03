9830cee970a81721eead52a031aa637fa361a6f5
Nataliia-Shevchenko <nata_yuna@ukr.net>
2023-03-03 23:18:29 +0200

    add task 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,32 +5,49 @@ const galleryEl = document.querySelector(".gallery");
 
 const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
 
-galleryEl.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsMarkup);
 
 function createGalleryItemsMarkup(items) {
-  return galleryItems.map(({ preview, original, description }) => {
-    return `<div class="gallery__item">
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
         <a class="gallery__link" href="${original}">
           <img
             class="gallery__image"
             src="${preview}"
-            data-source="${preview}"
+            data-source="${original}"
             alt="${description}"
           />
         </a>
         </div>`;
-  }).join('');
-
+    })
+    .join("");
 }
 
-galleryEl.addEventListener('click', handleGalleryItemClick);
+galleryEl.addEventListener("click", handleGalleryItemClick);
 
-function handleGalleryItemClick(evt){
-    evt.preventDefault();
-    if(!evt.target.classList.contains('gallery__image')){
-        return;
-    }
-    console.log(evt.target);
+function handleGalleryItemClick(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+     <img 
+    src="${evt.target.dataset.source}"
+  />
+  
+`);
+  instance.show();
+ 
 }
 
-console.log(galleryItems);
+// const instanceEl = document.querySelector('.modal__img')
+
+// instanceEl.addEventListener('keydown', handleEscapePress);
+
+// function handleEscapePress (evt) {
+//   console.log("key: ", evt.key);
+//   console.log("code: ", evt.code);
+// }
+
